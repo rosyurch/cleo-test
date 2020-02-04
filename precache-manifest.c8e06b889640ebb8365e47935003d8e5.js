6c0bb7e6f5c78ed2a6e2308f7fc4c37c607e68a8
Rostyslav Yurchyk <ros_ur@ukr.net>
2020-02-04 14:03:13 +0100

    Updates

---
+++ precache-manifest.c8e06b889640ebb8365e47935003d8e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aaba149aae1e67a1b0872e02604bcaf",
+    "url": "/cleo-test/index.html"
+  },
+  {
+    "revision": "6ee5b27bc9b93fab6a76",
+    "url": "/cleo-test/static/js/2.640d84a4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cleo-test/static/js/2.640d84a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9b0b3aa94abab1d0a0",
+    "url": "/cleo-test/static/js/main.fc2e52ae.chunk.js"
+  },
+  {
+    "revision": "b0c44b8bb9f268a5e1d7",
+    "url": "/cleo-test/static/js/runtime-main.4e567a5f.js"
+  }
+]);
